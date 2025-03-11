e47230c8717aeeb8961cef275b56ac0b745cfa0f
Océane Koska <oceane.koska@atos.net>
2025-03-11 11:07:32 +0100

    Fix namespace of utils functions

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,13 +2,15 @@
 #include <array>
 #include <cmath>
 
+#include "qpragma/hhl/utils.hpp"
+
 /* Get the sign of a double*/
-double sign(const double & d) {
+double qpragma::hhl::utils::sign(const double & d) {
     return d > 0. ? 1. : -1.;
 }
 
 /* Convert the binary value to a double flotting point value */
-double bin_to_double(uint64_t nb_bits, uint64_t val) {
+double qpragma::hhl::utils::bin_to_double(uint64_t nb_bits, uint64_t val) {
     if (val == 0) {
         return 1.;  // 0.000 and 1.000 equivalent but only 1. can be eigenvalue
     }
