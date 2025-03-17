a5355151738637eecea9bba5ff367204c82d9312
Océane Koska <oceane.koska@atos.net>
2025-03-17 11:25:35 +0100

    Fix 1 and 2 qubit cases

--- include/qpragma/hhl/stateprep.hpp
+++ include/qpragma/hhl/stateprep.hpp
@@ -32,11 +32,14 @@
 #include <cstdint>
 #include <cmath>
 
+#include <iostream>
+
 #include "qpragma.h"
 #include "qpragma/hhl/utils.hpp"
 
 
 namespace qpragma::hhl::stateprep {
+    const double _TOL = 1e-8;
 
     /**
      * TREE APPROACH STATE PREP
@@ -53,40 +56,52 @@ namespace qpragma::hhl::stateprep {
     #pragma quantum routine (std::array<double, (1 << SIZE)> init_array)
     template <uint64_t SIZE>
     void kp_tree(const array<SIZE> & qreg) {
+        // 1-qubit system
         if constexpr (SIZE == 1UL) {
-             double angle = 2. * qpragma::hhl::utils::sign(init_array[2]) 
-                               * acos(qpragma::hhl::utils::sign(init_array[1] * sqrt(init_array[1])));
-             (RY(angle))(qreg);
+            double angle = 2. * qpragma::hhl::utils::sign(init_array[1]) 
+                               * acos(qpragma::hhl::utils::sign(init_array[0]) * init_array[0]);
+            (RY(angle))(qreg);
         }
-
+        // Multi-qubit system
         else {
             std::vector<double> tree_vect = qpragma::hhl::stateprep::get_tree_coeff<SIZE>(init_array);
+            for (auto e : tree_vect) {
+                std::cout << e << std::endl;
+            }
 
             // First rotation on the first qubit
             double angle = 2 * acos(sqrt(tree_vect[1]));
-            (RY(angle))(qreg[0]);
+            (RY(angle))(qreg[SIZE - 1UL]);
 
             // Iter until the leaves (excluded) are reached --> work only with proba
+            uint64_t start_val;
+            double left, right;
             for (uint64_t idx = 1 ; idx < SIZE - 1 ; ++idx) {
-                uint64_t start_val = (1 << (idx + 1)) - 1;
+                start_val = (1 << (idx + 1)) - 1;
 
                 for (uint64_t ctrl_val = 0 ; ctrl_val < (1 << idx) ; ++ctrl_val) {
-                    angle = 2 * acos(sqrt(tree_vect[start_val + 2 * ctrl_val]));
+                    left = tree_vect[start_val + 2 * ctrl_val];
+                    right = tree_vect[start_val + 2 * ctrl_val + 1];
+                    angle = 2 * acos(sqrt(left / (left + right)));
 
-                    #pragma quantum ctrl (qpragma::as_uint<SIZE>(qreg(0, idx-1)) == ctrl_val)
-                    (RY(angle))(qreg[idx]);
+                    #pragma quantum ctrl (qpragma::as_uint<SIZE>(qreg(SIZE-1, SIZE-1-idx,-1)) == ctrl_val)
+                    (RY(angle))(qreg[SIZE - 1UL - idx]);
                 }
             }
 
-            // Last iteration : take into account signs
-            for (uint64_t ctrl_val = 0 ; ctrl_val << (1 < SIZE) ; ++ctrl_val) {
-                angle = 2 * qpragma::hhl::utils::sign(init_array[2 * ctrl_val + 1]) * acos(
-                    qpragma::hhl::utils::sign(init_array[2 * ctrl_val]) * sqrt(tree_vect[ctrl_val + 2 * ctrl_val])
-                );
-                #pragma quantum ctrl (qpragma::as_uint<SIZE>(qreg(0, SIZE-2)) == ctrl_val)
-                (RY(angle))(qreg[SIZE - 1]);
+            // Last iteration : take into account signs from init_array
+            start_val = (1 << SIZE) - 1;
+            for (uint64_t ctrl_val = 0 ; ctrl_val < 1 << (1 < SIZE) ; ++ctrl_val) {
+                double sign_left = qpragma::hhl::utils::sign(init_array[2 * ctrl_val]);
+                double sign_right = qpragma::hhl::utils::sign(init_array[2 * ctrl_val + 1]);
+                left = tree_vect[start_val + 2 * ctrl_val];
+                right = tree_vect[start_val + 2 * ctrl_val + 1];
+                if (left + right > _TOL) {
+                    angle = 2 * sign_right * acos(sign_left * sqrt(left / (left + right)));
+                    #pragma quantum ctrl (qpragma::as_uint<SIZE>(qreg(SIZE - 1, 1, -1)) == ctrl_val)
+                    (RY(angle))(qreg[0]);
+                }
             }
-
         }
     }
 }   // qpragma::hhl::stateprep
