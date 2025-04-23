bc2fd8b9acb39e316221c6b3c859606dc2582337
Océane Koska <oceane.koska@atos.net>
2025-04-23 15:41:18 +0200

    Add tests for hybrid HHL, matrices of size 2x2 + easy use cases for 4x4 matrices

--- tests/test_hhl.cpp
+++ tests/test_hhl.cpp
@@ -9,6 +9,9 @@ using namespace qpragma;
 
 double ERROR_RATIO = 0.1;
 
+
+/* HHL*/
+
 /* 1 qubit - 2x2 matrices */
 
 
@@ -207,6 +210,123 @@ TEST(HHL, m2x2_usecase1) {
         // Sampling on HHL algorithm
         auto res = hhl::test_hhl<SIZE, SIZE_C>(init_array, obs, NB_SHOTS);
     
+        for (uint64_t idx = 0UL ; idx < (1UL << SIZE) ; ++idx) {
+            ASSERT_NEAR((double) res[idx] / NB_SHOTS, expected_result[idx], ERROR_RATIO);
+        }
+    }
+}
+
+TEST(HHL, m2x2_usecase2) {
+    constexpr uint64_t SIZE = 1UL;
+    constexpr uint64_t SIZE_C = 4UL;
+    constexpr uint64_t NB_SHOTS = 100UL;
+
+    /* A = 0.5 I + 0.25 Z */
+    hhl::observables::PauliTerm<SIZE> term1 { "I", 0.5 };
+    hhl::observables::PauliTerm<SIZE> term2 { "Z", 0.25};
+    std::vector term_vect { term1, term2 };
+    hhl::observables::Observable<SIZE> obs { term_vect };
+
+    {
+        // b = |0> + |1>
+        std::array<double, 1UL << SIZE> init_array {1., 1.};
+        qpragma::hhl::utils::normalize<SIZE>(init_array);
+
+        // Expected result
+        std::array<double, 1UL << SIZE> expected_result {0.10, 0.90};
+
+
+        // Sampling on HHL algorithm
+        auto res = hhl::test_hhl<SIZE, SIZE_C>(init_array, obs, NB_SHOTS);
+
+        for (uint64_t idx = 0UL ; idx < (1UL << SIZE) ; ++idx) {
+            ASSERT_NEAR((double) res[idx] / NB_SHOTS, expected_result[idx], ERROR_RATIO);
+        }
+    }
+}
+
+TEST(HHL, m2x2_usecase3) {
+    constexpr uint64_t SIZE = 1UL;
+    constexpr uint64_t SIZE_C = 4UL;
+    constexpr uint64_t NB_SHOTS = 100UL;
+
+    /* A = 0.5 I + 0.125 X */
+    hhl::observables::PauliTerm<SIZE> term1 { "I", 0.5 };
+    hhl::observables::PauliTerm<SIZE> term2 { "X", 0.125};
+    std::vector term_vect { term1, term2 };
+    hhl::observables::Observable<SIZE> obs { term_vect };
+
+    {
+        // b = |0>
+        std::array<double, 1UL << SIZE> init_array {1., 0.};
+
+        // Expected result
+        std::array<double, 1UL << SIZE> expected_result {0.94, 0.06};
+
+
+        // Sampling on HHL algorithm
+        auto res = hhl::test_hhl<SIZE, SIZE_C>(init_array, obs, NB_SHOTS);
+
+        for (uint64_t idx = 0UL ; idx < (1UL << SIZE) ; ++idx) {
+            ASSERT_NEAR((double) res[idx] / NB_SHOTS, expected_result[idx], ERROR_RATIO);
+        }
+    }
+}
+
+
+/* 2 qubits - 4x4 matrices */
+
+
+TEST(HHL, m4x4_II) {
+    constexpr uint64_t SIZE = 2UL;
+    constexpr uint64_t SIZE_C = 4UL;
+    constexpr uint64_t NB_SHOTS = 100UL;
+
+    /* A = 0.5 II */
+    hhl::observables::PauliTerm<SIZE> term1 { "II", 0.5 };
+    std::vector term_vect { term1 };
+    hhl::observables::Observable<SIZE> obs { term_vect };
+
+    for (uint64_t val = 0UL ; val < (1UL << SIZE) ; ++val) {
+        // b = |val>
+        std::array<double, 1UL << SIZE> init_array {0., 0., 0., 0.};
+        init_array[val] = 1.;
+
+        // Expected result
+        std::array<double, 1UL << SIZE> expected_result {0., 0., 0., 0.};
+        expected_result[val] = 1.;
+
+        // Sampling on HHL algorithm
+        auto res = hhl::test_hhl<SIZE, SIZE_C>(init_array, obs, NB_SHOTS);
+
+        for (uint64_t idx = 0UL ; idx < (1UL << SIZE) ; ++idx) {
+            ASSERT_NEAR((double) res[idx] / NB_SHOTS, expected_result[idx], ERROR_RATIO);
+        }
+    }
+}
+
+TEST(HHL, m4x4_XX) {
+    constexpr uint64_t SIZE = 2UL;
+    constexpr uint64_t SIZE_C = 4UL;
+    constexpr uint64_t NB_SHOTS = 100UL;
+
+    /* A = 0.5 II */
+    hhl::observables::PauliTerm<SIZE> term1 { "XX", 0.5 };
+    std::vector term_vect { term1 };
+    hhl::observables::Observable<SIZE> obs { term_vect };
+
+    for (uint64_t val = 0UL ; val < (1UL << SIZE) ; ++val) {
+        // b = |val>
+        std::array<double, 1UL << SIZE> init_array {0., 0., 0., 0.};
+        init_array[val] = 1.;
+
+        // Expected result
+        std::array<double, 1UL << SIZE> expected_result {0., 0., 0., 0.};
+        expected_result[(1UL << SIZE) - val - 1UL] = 1.;
+
+        // Sampling on HHL algorithm
+        auto res = hhl::test_hhl<SIZE, SIZE_C>(init_array, obs, NB_SHOTS);
+
         for (uint64_t idx = 0UL ; idx < (1UL << SIZE) ; ++idx) {
             ASSERT_NEAR((double) res[idx] / NB_SHOTS, expected_result[idx], ERROR_RATIO);
         }
